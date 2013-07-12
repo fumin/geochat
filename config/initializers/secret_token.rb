8bec80d963b3eabcbab681aea5f4e8a1577e02c9
awaw <awaw@nandalu.idv.tw>
2013-07-12 20:45:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeoChat::Application.config.secret_key_base = '49f2cc8c8c705845ebd4247239322a71661163c0f96c1378b7855101604f4f5dff39bee4c77dfac000d88a6d39079fe4f749d80ea1fae2be7ec9ef08d6c79030'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeoChat::Application.config.session_store :cookie_store, key: '_geo_chat_session'
