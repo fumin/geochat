a5e1e183baa53eab90f33553f2dd30b919f4c99b
awaw <awaw@nandalu.idv.tw>
2013-07-12 22:12:09 +0800

    configs

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,11 @@
-Redis.current = Redis.new url: ENV['OPENREDIS_URL']
+url = if Rails.env == 'production'
+        # openshift
+        "redis://:ZTNiMGM0NDI5OGZjMWMxNDlhZmJmNGM4OTk2ZmI5@127.6.23.3:16379"
+      else
+        nil
+      end
+
+Redis.current = Redis.new url: url
 
 if 'test' == Rails.env
   Redis.current.select('9')
